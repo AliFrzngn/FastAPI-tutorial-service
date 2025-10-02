28b1b8b069b3a438743796c0e1d98519033c6680
alilocal <ahiborat@gmail.com>
2025-10-02 10:11:59 +0330

    Add exception handling for HTTP and validation errors; improve error response structure in main application.

--- core/main.py
+++ core/main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 from tasks.routes import router as tasks_routes
 from users.routes import router as users_routes
 from fastapi.middleware.cors import CORSMiddleware
 import time
 from contextlib import asynccontextmanager
 import os
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.exceptions import RequestValidationError
 
 tags_metadata = [
     {
@@ -51,7 +53,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#app.include_router(tasks_routes, prefix="/api/v1")
+# app.include_router(tasks_routes, prefix="/api/v1")
 app.include_router(tasks_routes)
 # app.include_router(users_routes, prefix="/api/v1")
 app.include_router(users_routes)
@@ -61,13 +63,17 @@ static_path = os.path.join(os.path.dirname(__file__), "static")
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 
 # Routes for HTML pages
+
+
 @app.get("/login")
 async def login_page():
     return FileResponse(os.path.join(static_path, "login.html"))
 
+
 @app.get("/dashboard")
 async def dashboard_page():
     return FileResponse(os.path.join(static_path, "dashboard.html"))
+
 
 @app.get("/")
 async def root():
@@ -81,3 +87,26 @@ async def add_process_time_header(request: Request, call_next):
     process_time = time.perf_counter() - start_time
     response.headers["X-Process-Time"] = str(process_time)
     return response
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handelet(request, exc):
+    error_response = {
+        "error": True,
+        "status_code": exc.status_code,
+        "detail": exc.detail,
+    }
+    return JSONResponse(status_code=exc.status_code, content=error_response)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    error_response = {
+        "error": True,
+        "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
+        "detail": "Problems parsing the request body",
+        "content": exc.body,
+        "reason": exc.errors,
+        "errors": exc.errors(),
+    }
+    return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, content=error_response)
